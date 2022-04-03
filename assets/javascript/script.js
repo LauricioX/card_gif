3523cc33a23ffc5dff9695f84356da30ee40cfd5
Lauricio De Souza <lauriciomalek@gmail.com>
2022-04-03 00:40:09 -0300

    modal

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+let btnmodal = document.querySelector("#btn__modal");
+let modal = document.querySelector("#add__modal");
+
+btnmodal.addEventListener("click", function(){
+    modal.classList.toggle("show")
+})
